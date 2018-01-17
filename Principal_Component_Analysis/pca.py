411888d29bb2e7fd89b7887d900309468008b0b6
hemanthbd <fhemanth@asu.edu>
2018-01-16 23:32:34 -0700

    PCA commit

---
+++ Principal_Component_Analysis/pca.py
@@ -0,0 +1,36 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
+import seaborn as sns
+
+from sklearn.datasets import load_breast_cancer
+
+cancer = load_breast_cancer()
+
+df = pd.DataFrame(cancer['data'],columns=cancer['feature_names'])
+
+from sklearn.preprocessing import StandardScaler
+
+scaler = StandardScaler()
+scaler.fit(df)
+scaled_data = scaler.transform(df)
+
+from sklearn.decomposition import PCA
+
+pca = PCA(n_components=2)
+
+pca.fit(scaled_data)
+
+x_pca = pca.transform(scaled_data)
+
+plt.figure(figsize=(8,6))
+plt.scatter(x_pca[:,0],x_pca[:,1],c=cancer['target'],cmap='plasma')
+plt.xlabel('First principal component')
+plt.ylabel('Second Principal Component')
+
+
+df_comp = pd.DataFrame(pca.components_,columns=cancer['feature_names'])
+plt.figure(figsize=(12,6))
+sns.heatmap(df_comp,cmap='plasma',)
+
+
